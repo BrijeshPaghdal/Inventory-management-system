f0bdf4d2b21d1ec7395a32864a65d972ea1855e6
brijeshpaghdal <brijeshpaghdal13@gmail.com>
2022-09-15 02:04:49 +0530

    update and delete

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import configparser
+from math import prod
+from random import randint
 import flask
 from bson import ObjectId
 from flask import Flask, render_template, request, jsonify, redirect, url_for
 from pymongo import MongoClient
 import json
-
+import random
 version = "0.0.1"
 
 app = Flask(__name__)
@@ -22,71 +24,99 @@ location = db["location"]
 
 @app.route('/')
 def index():
-    return render_template('index.html', config=config)
+    data = fetchData(product)
+    return render_template('index.html', data=data, config=config)
 
 
-@app.route('/error/<string:e>')
-def error(e):
-    return render_template('error.html', error=e)
-
-
-@app.route('/add-product', methods=['GET','POST'])
+@app.route('/add-product', methods=['GET', 'POST'])
 def addProductPage():
     return render_template('add-product.html')
 
-@app.route('/ajax-add-product',methods=['GET', 'POST'])
+
+@app.route('/remove-product', methods=['GET', 'POST'])
+def removeProduct():
+    if request.method == "POST":
+        data = request.prod_id
+        print(data)
+
+
+@app.route('/ajax-add-product', methods=['GET', 'POST'])
 def addProduct():
     if request.method == "POST":
         data = request.get_json()
-        data = {"_id": data['prod_id'] ,
+        data = {"_id": data['prod_id'],
                 "prod_name": data['prod_name'],
                 "price": data['price'],
                 "quantity": data['quantity']}
-        results = addtoDB(product,data)
-    return jsonify(results)
-
-@app.route('/edit-product',methods=['GET', 'POST'])
-def editProductPage():
-    return render_template('edit-product.html')
-
-
-@app.route('/location')
-def locationPage():
-    data = fetchData(location)
-    return render_template('location.html',data=data)
-
-@app.route('/ajax-add-location',methods=['GET', 'POST'])
-def addLocation():
-    if request.method == "POST":
-        data = request.get_json()
-        # print(data)
-        data = {"location_name": data['loc_name']}
-        results = addtoDB(location,data)
-    return jsonify(results)
-
-@app.route('/remove-location',methods=['GET', 'POST'])
-def removeLocation():
-    if request.method == "POST":
-        data = request.get_json()
-        id = data['loc_id']
-        results = removeFromDB(location,id)
+        results = addtoDB(product, data)
     return jsonify(results)
 
 
-def removeFromDB(table, id):
+@app.route('/edit-product/<string:id>', methods=['GET', 'POST'])
+def editProductPage(id):
+    print(type(id))
+
+    data = product.find_one({"_id": str(id)})
+    # print(data)
+    return render_template('edit-product.html', data=data, id=id)
+
+
+@app.route('/update-product', methods=['GET', 'POST'])
+def editProduct():
     try:
-        x = table.delete_one({"_id": id})
+        if request.method == "POST":
+            data = request.get_json()
+            result = product.find_one_and_update(
+                {'_id': str(data['old_prod_id'])}, {"$set": {"_id": data['prod_id'],
+                                                             "prod_name": data['prod_name'],
+                                                             "price": data['price'],
+                                                             "quantity": data['quantity']}})
+
+        print(result)
         results = {'Result': 'Success'}
     except:
         results = {'Result': 'Failed'}
     return results
 
 
-def updateProductPage():
-    return
+@app.route('/location')
+def locationPage():
+    data = fetchData(location)
+    return render_template('location.html', data=data)
 
 
-def addtoDB(table,data):
+@app.route('/ajax-add-location', methods=['GET', 'POST'])
+def addLocation():
+    if request.method == "POST":
+        data = request.get_json()
+        # print(data)
+        data = {"_id": random.randint(
+            1, 10000), "location_name": data['loc_name']}
+        results = addtoDB(location, data)
+    return jsonify(results)
+
+
+@app.route('/remove-location', methods=['GET', 'POST'])
+def removeLocation():
+    if request.method == "POST":
+        data = request.get_json()
+        id = data['loc_id']
+        print(type(id))
+        results = removeFromDB(location, id)
+    return jsonify(results)
+
+
+def removeFromDB(table, id):
+    x = table.delete_one({"_id": id})
+    print(x)
+    try:
+        results = {'Result': 'Success'}
+    except:
+        results = {'Result': 'Failed'}
+    return results
+
+
+def addtoDB(table, data):
     try:
         table.insert_one(data)
         results = {'Result': 'Success'}
@@ -94,9 +124,11 @@ def addtoDB(table,data):
         results = {'Result': 'Failed'}
     return results
 
+
 def fetchData(table):
     data = table.find()
     return (todo for todo in data)
 
+
 if __name__ == "__main__":
-    app.run(host="127.0.0.1",debug=True)
+    app.run(host="127.0.0.1", debug=True)
--- static/js/custom.js
+++ static/js/custom.js
@@ -7,6 +7,7 @@ $(document).ready(function () {
   var url = url.split('/');
   var url = url[url.length - 1];
 
+
   //===================================== PRODUCT LIST =====================================//
 
   if (url.split('?').includes('')) {
@@ -22,6 +23,8 @@ $(document).ready(function () {
       });
     });
 
+
+
     function showCancelMessage(productId) {
       swal({
         title: "Are you sure?",
@@ -36,7 +39,7 @@ $(document).ready(function () {
       }, function (isConfirm) {
         if (isConfirm) {
           $.ajax({
-            url: './ajax/ajax-delete-product.php',
+            url: 'remove-product',
             type: 'POST',
             data: {
               prod_id: productId
@@ -60,70 +63,56 @@ $(document).ready(function () {
   }
 
   //===================================== Edit PRODUCT =====================================//
-//
-//  if (url.split('?').includes('edit-product.php')) {
-//    $(document).on("change", "#Cate_Name", function (e) {
-//      val = this.value;
-//      getSubCate(val);
-//    });
-//    if ($("#Sub_C_Name").val() == null) {
-//      getSubCate($("#Cate_Name").val());
-//    }
-//
-//    function getSubCate(val1) {
-//      $.ajax({
-//        url: "ajax/ajax-select-sub-cate.php",
-//        type: "POST",
-//        data: {
-//          cate_name: val1
-//        },
-//        success: function (data) {
-//          $("#Sub_C_Name").html(data);
-//        }
-//      });
-//    }
-//
-//    $("#formEdit").on("submit", function (e) {
-//
-//      e.preventDefault();
-//
-//      var formData = new FormData(this);
-//
-//      $.ajax({
-//
-//        url: "./ajax/ajax-edit-product.php",
-//        type: "POST",
-//        data: formData,
-//        contentType: false,
-//        processData: false,
-//        success: function (data) {
-//          if (data == 1) {
-//            swal({
-//              title: "Updated!",
-//              text: "Your product updated succesfully.",
-//              type: "success",
-//              showCancelButton: false,
-//              confirmButtonColor: "#DD6B55",
-//              confirmButtonText: "ok",
-//              closeOnConfirm: false,
-//              closeOnCancel: false
-//            }, function (isConfirm) {
-//              if (isConfirm) {
-//                document.location.href = "http://localhost/phpproj/EasyGift/vendor/product-list.php";
-//              }
-//            });
-//          } else if (data == 2) {
-//            swal("Cancelled", "Image upload failed", "warning");
-//          } else if (data == 3) {
-//            swal("Cancelled", "Image upload failed", "warning");
-//          } else {
-//            swal("Cancelled", "Product Update Failed!!", "warning");
-//          }
-//        }
-//      });
-//
-//    });
-//  }
+
+  var url1 = window.location.href;
+  var url1 = url1.split('/');
+  var url1 = url1[url1.length - 2];
+
+  if (url1.split('?').includes('edit-product')) {
+    $("#formEdit").on("submit", function (e) {
+      e.preventDefault();
+
+      var server_data = {
+        "prod_id": document.getElementById("Prod_Id").value,
+        "prod_name": document.getElementById("Prod_Name").value,
+        "price": document.getElementById("Price").value,
+        "quantity": document.getElementById("Avail_Qua").value,
+        "old_prod_id": $("#update-product").data('id')
+      };
+
+      $.ajax({
+        url: "/update-product",
+        type: "POST",
+        data: JSON.stringify(server_data),
+        contentType: "application/json",
+        dataType: 'json',
+        success: function (data) {
+          if (data.Result == 'Success') {
+            swal({
+              title: 'Updated',
+              text: 'Product Inserted successfully',
+              type: 'success',
+              showCancelButton: false,
+              confirmButtonColor: '#DD6B55',
+              confirmButtonText: 'OK',
+              closeOnConfirm: false,
+              closeOnCancel: false,
+            }, function (isConfirm) {
+              if (isConfirm) {
+                window.location.href = "/";
+              }
+            });
+
+          } else {
+            swal("Failed", "Something went wrong", "error");
+            $("#formAddProduct").trigger("reset");
+          }
+
+        }
+      });
+
+    });
+  }
 
   //===================================== ADD PRODUCT =====================================//
 
@@ -132,35 +121,37 @@ $(document).ready(function () {
       e.preventDefault();
       var formData = new FormData(this);
 
-        var server_data = {"prod_id": document.getElementById("Prod_Id").value,
-          "prod_name": document.getElementById("Prod_Name").value,
-          "price": document.getElementById("Price").value,
-          "quantity": document.getElementById("Avail_Qua").value};
+      var server_data = {
+        "prod_id": document.getElementById("Prod_Id").value,
+        "prod_name": document.getElementById("Prod_Name").value,
+        "price": document.getElementById("Price").value,
+        "quantity": document.getElementById("Avail_Qua").value
+      };
 
-          $.ajax({
-            url: "/ajax-add-product",
-            type: "POST",
-            data: JSON.stringify(server_data),
-            contentType: "application/json",
-            dataType: 'json',
-            success: function (data) {
-              if (data.Result == 'Success' ) {
-                swal("Inserted", "Product Inserted successfully", "success");
-                $("#formAddProduct").trigger("reset");
-              } else {
-                swal("Failed", "Something went wrong", "error");
-                $("#formAddProduct").trigger("reset");
+      $.ajax({
+        url: "/ajax-add-product",
+        type: "POST",
+        data: JSON.stringify(server_data),
+        contentType: "application/json",
+        dataType: 'json',
+        success: function (data) {
+          if (data.Result == 'Success') {
+            swal("Inserted", "Product Inserted successfully", "success");
+            $("#formAddProduct").trigger("reset");
+          } else {
+            swal("Failed", "Something went wrong", "error");
+            $("#formAddProduct").trigger("reset");
 
-              }
+          }
 
-            }
-          });
+        }
+      });
 
     });
 
   }
 
-  //===================================== ORDER =====================================//
+  //===================================== Location =====================================//
 
   if (url.split('?').includes('location')) {
 
@@ -168,104 +159,101 @@ $(document).ready(function () {
       location.reload()
     }
 
-     $("#formAddLocation").on("submit", function (e) {
+    $("#formAddLocation").on("submit", function (e) {
       e.preventDefault();
       var formData = new FormData(this);
 
-        var server_data = {"loc_name": document.getElementById("locname").value};
+      var server_data = { "loc_name": document.getElementById("locname").value };
 
-          $.ajax({
-            url: "/ajax-add-location",
-            type: "POST",
-            data: JSON.stringify(server_data),
-            contentType: "application/json",
-            dataType: 'json',
-            success: function (data) {
-              if (data.Result == 'Success' ) {
-                swal({
-                  title: 'Inserted',
-                  text: 'Location Added successfully',
-                  type: 'success',
-                  showCancelButton: false,
-                  confirmButtonColor: '#DD6B55',
-                  confirmButtonText: 'OK',
-                  closeOnConfirm: false,
-                  closeOnCancel: false,
-                },function(isConfirm){
-                 if (isConfirm) {
-                    $("#formAddLocation").trigger("reset");
-                    showData();
-                 }
-                });
-
-
-              } else {
-                swal("Failed", "Something went wrong", "error");
+      $.ajax({
+        url: "/ajax-add-location",
+        type: "POST",
+        data: JSON.stringify(server_data),
+        contentType: "application/json",
+        dataType: 'json',
+        success: function (data) {
+          if (data.Result == 'Success') {
+            swal({
+              title: 'Inserted',
+              text: 'Location Added successfully',
+              type: 'success',
+              showCancelButton: false,
+              confirmButtonColor: '#DD6B55',
+              confirmButtonText: 'OK',
+              closeOnConfirm: false,
+              closeOnCancel: false,
+            }, function (isConfirm) {
+              if (isConfirm) {
+                $("#formAddLocation").trigger("reset");
+                showData();
               }
-            }
-          });
+            });
+
+
+          } else {
+            swal("Failed", "Something went wrong", "error");
+          }
+        }
+      });
 
     });
 
-//======================= Remove location =============================
+    //======================= Remove location =============================
     ('use strict');
-    $(function() {
-      $(document).on('click', '#btnLocDelete', function(e) {
+    $(function () {
+      $(document).on('click', '#btnLocDelete', function (e) {
         e.preventDefault();
-        var type = $(this).data('type');
         var loc_id = $(this).data('id');
-        console.log(loc_id)
-        if (type === 'confirm') {
-          showCancelMessage(loc_id);
-        }
+        console.log(loc_id);
+        showCancelMessage(loc_id);
       });
     });
     function showCancelMessage(loc_id) {
-    var server_data = {"loc_id": loc_id}
+      var server_data = { "loc_id": loc_id };
       swal({
-          title: 'Are you sure?',
-          text: 'You will not be able to recover this Location detail!',
-          type: 'warning',
-          showCancelButton: true,
-          confirmButtonColor: '#DD6B55',
-          confirmButtonText: 'Yes, delete it!',
-          cancelButtonText: 'No, cancel!',
-          closeOnConfirm: false,
-          closeOnCancel: false,
-        },
-        function(isConfirm) {
+        title: 'Are you sure?',
+        text: 'You will not be able to recover this Location detail!',
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#DD6B55',
+        confirmButtonText: 'Yes, delete it!',
+        cancelButtonText: 'No, cancel!',
+        closeOnConfirm: false,
+        closeOnCancel: false,
+      },
+        function (isConfirm) {
           if (isConfirm) {
             $.ajax({
-             url: '/remove-location',
-                type: 'POST',
-                data: JSON.stringify(server_data),
-                contentType: "application/json",
-                dataType: 'json',
-                  success: function(data) {
-                    if (data.Result == 'Success' ) {
-                      swal({
-                          title: 'Deleted!',
-                          text: 'Location Deleted Successfully',
-                          type: 'success',
-                          showCancelButton: false,
-                          confirmButtonColor: '#DD6B55',
-                          confirmButtonText: 'OK',
-                          closeOnConfirm: false,
-                          closeOnCancel: false,
-                        },function(isConfirm){
-                         if (isConfirm) {
-                            showData();
-                         }
-                        });
-                    } else {
-                      swal(
-                        'Cancelled',
-                        'Error Occured in deleting Location',
-                        'error'
-                      );
+              url: '/remove-location',
+              type: 'POST',
+              data: JSON.stringify(server_data),
+              contentType: "application/json",
+              dataType: 'json',
+              success: function (data) {
+                if (data.Result == 'Success') {
+                  swal({
+                    title: 'Deleted!',
+                    text: 'Location Deleted Successfully',
+                    type: 'success',
+                    showCancelButton: false,
+                    confirmButtonColor: '#DD6B55',
+                    confirmButtonText: 'OK',
+                    closeOnConfirm: false,
+                    closeOnCancel: false,
+                  }, function (isConfirm) {
+                    if (isConfirm) {
+                      location.reload();
                     }
-                  }
-             });
+                  });
+                } else {
+                  swal(
+                    'Cancelled',
+                    'Error Occured in deleting Location',
+                    'error'
+                  );
+                }
+              }
+            });
           } else {
             swal('Cancelled', 'Location is Not Deleted', 'error');
           }
@@ -276,24 +264,10 @@ $(document).ready(function () {
 
   //--------------------------functions -----------------------------------//
 
-  function json(url) {
-    return fetch(url).then(res => res.json());
-  }
-  var temp = 0;
-
-
-
-
-
-  function getQuickProductView() {
-    $.ajax({
-      url: "ajax/ajax-quick-product-view.php",
-      type: "POST",
-      success: function (data) {
-        $('#quick-product-view').html(data);
-      }
-    });
-  }
+  // function json(url) {
+  //   return fetch(url).then(res => res.json());
+  // }
+  // var temp = 0;
 
 
   function checkConfirmPassswd(password1, password2) {
